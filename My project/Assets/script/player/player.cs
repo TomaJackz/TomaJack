5ec6cf1e8bd37809c77c2babda069094e1a6a656
TomaJackz <caua.arthur@escolar.ifrn.edu.br>
2025-05-12 11:50:34 -0300

    BOLADÃO

---
+++ My project/Assets/script/player/player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private int numero;
+    public float velocidade = 40;
+    
+    void Start()
+    {
+        numero = 0;
+    }
+    
+    // Update is called one per frame
+    void Update()
+    {
+        // debug.log(numero) ;
+        // numero = numero + 1 ;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
+    }
+}
